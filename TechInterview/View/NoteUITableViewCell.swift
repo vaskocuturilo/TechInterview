1ded029c8d66c5d5d91fb6419c43c0979be903b1
vaskocuturilo <vaskocuturilo@rambler.ru>
2022-01-23 22:23:11 +0300

    DV-00010: Remove some cells

--- TechInterview/View/NoteUITableViewCell.swift
+++ TechInterview/View/NoteUITableViewCell.swift
@@ -8,17 +8,8 @@
 import UIKit
 
 class NoteUITableViewCell: UITableViewCell {
-
+    
     private(set) var noteTitle : String = ""
-    private(set) var noteText  : String = ""
-    private(set) var noteDate  : String = ""
     
-   // @IBOutlet weak var noteTitleLabel: UILabel!
-    //@IBOutlet weak var noteDateLabel: UILabel!
-    //@IBOutlet weak var noteTextLabel: UILabel!
-
     @IBOutlet weak var noteTitleLabel: UILabel!
-    
-    @IBOutlet weak var noteTextLabel: UILabel!
-    @IBOutlet weak var noteDateLabel: UILabel!
 }
